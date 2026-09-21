4c2138960f4ce61f34137381e9741a585cd8ef08
alaemaghchich <alaemaghchich2004@gmail.com>
2026-09-21 23:26:13 +0200

    add bzero ,  fill n time byte by byte in memory

--- libft.h
+++ libft.h
@@ -15,5 +15,7 @@ size_t ft_strlen(const char *s);
 void *ft_memset(void *s, int c, size_t n);
 int ft_toupper(int c);
 int ft_tolower(int c);
+int ft_atoi(const char *nptr);
+void ft_bzero(void *s, size_t n);
 
 #endif
